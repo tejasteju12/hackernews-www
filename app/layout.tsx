6d2def9805edf5cd46c6b3007b19a00638d04a97
tejasteju12 <steja.1si21ad059@gmail.com>
2025-05-15 16:16:17 +0530

    fix: update description in metadata to reflect correct author name

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 
 export const metadata = {
   title: "Hacker News",
-  description: "Built by Kethan",
+  description: "Built by S Teja",
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => {
